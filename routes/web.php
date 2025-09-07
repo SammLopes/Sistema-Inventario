89e5b2a9c2a64c1db467f2721c2a7f9822e529b4
SammLopes <sam.v.p.lopes@gmail.com>
2025-09-07 20:02:01 -0300

    Adicioando rotas no web.php midldewares de guest e auth e rotas de login, logout e produtos.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Auth::check() ? redirect('/produtos') : redirect('auth.login');
+});
+
+Route::middleware('guest')->group( function() {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+});
+
+Route::middleware('auth')->group( function(){
+      Route::get('/produtos', [ProductController::class, 'index'])->name('produtos.index');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
