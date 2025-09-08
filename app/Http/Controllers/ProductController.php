6d52ac72e848bd3c2ad64586ed2b65b41c2f3536
SammLopes <sam.v.p.lopes@gmail.com>
2025-09-07 22:24:27 -0300

    Adicionado funçẽos de index e update de dos produtos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\FakeStoreService;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    private $fakeStoreService;
+    
+    public function __construct(FakeStoreService $fakeStoreService)
+    {
+        $this->fakeStoreService = $fakeStoreService;
+    }
+    
+    public function index()
+    {
+        $products = Product::orderBy('name')->get();
+        return view('products.index', compact('products'));
+    }
+
+    public function updateStock(Request $request, Product $product)
+    {
+
+        $request->validate([
+            'stock' => 'required|integer|min:0'
+        ], [
+            'stock.required' => 'O estoque é obrigatório',
+            'stock.integer' => 'O estoque deve ser um número inteiro',
+            'stock.min' => 'O estoque não pode ser negativo'
+        ]);
+
+        $product->update([
+            'stock' => $request->stock
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Estoque atualizado com sucesso!',
+            'estoque' => $product->estoque
+        ]);
+    }
+
 }
