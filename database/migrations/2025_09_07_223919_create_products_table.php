67cbf2d49c7701cdaaa2cb75f68197082a2091a4
SammLopes <sam.v.p.lopes@gmail.com>
2025-09-07 21:00:50 -0300

    ALterado nome das colunas da talbeal de produtos e adicioando novas colunas

--- database/migrations/2025_09_07_223919_create_products_table.php
+++ database/migrations/2025_09_07_223919_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->integer('api_id')->nullable(); // ID da Fake Store API
+            $table->string('category')->nullable();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
